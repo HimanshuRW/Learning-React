c15e073145bbb2e44e05823d06bc13b7ce68d72d
Himanshu <himanshurw56@gmail.com>
2023-12-09 22:56:59 +0530

    remove bhai

---
+++ 10 Sorting Visualiser/try/quick.js
@@ -0,0 +1,13 @@
+let array = [5,3,7,6,1,2,4];
+
+function quick(arr) {
+    let pivot = arr[arr.lenght-1];
+    let i =0;
+    let j=0;
+    if(arr[i]>pivot){
+        while(arr[j]>pivot){
+            j++;
+        }
+        swap(i,j);
+    }
+}
